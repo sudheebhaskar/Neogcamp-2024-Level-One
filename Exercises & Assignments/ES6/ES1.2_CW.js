c4d7f31ff0d8db2d170453f2fc109fe3b6cd125a
Sudheendra Bhaskar <sudheebhaskar@gmail.com>
2024-09-30 04:51:57 +0530

    let, const and var usage & scope

---
+++ Exercises & Assignments/ES6/ES1.2_CW.js
@@ -0,0 +1,70 @@
+let a = 2;
+
+if(true){
+  console.log(a)
+}
+
+
+
+//let num = 100
+
+// function printNum(){
+//   let num = 40;
+//   console.log(num)
+// }
+
+// console.log(num)
+// printNum();
+
+
+//let num=100;
+
+// function printNum(){
+//   console.log(num)
+//   let num=10;
+//   console.log(num)
+// }
+
+// console.log(num);
+// printNum()
+
+
+
+let num=100;
+
+function printNum(){
+//  console.log(num)
+  let num = 40;
+  console.log(num)
+
+if(true){
+  let num = 50;
+  console.log(num)
+  }
+  console.log(num)
+}
+console.log(num)
+printNum()
+
+
+
+let num1 = 0;
+
+if(true){
+  num1= 20;
+  console.log(num1)
+}
+
+console.log(num1);
+
+
+var name = "Joe"
+console.log(name);
+
+console.log(name);
+var name = "Joe";
+
+
+var name;
+console.log(name);
+name = "Joe"
