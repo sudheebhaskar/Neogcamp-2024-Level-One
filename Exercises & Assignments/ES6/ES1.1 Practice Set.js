7860886f8b1423767e2a7b77e366c34fb416e471
Sudheendra Bhaskar <sudheebhaskar@gmail.com>
2024-09-30 04:48:33 +0530

    Folder structure updated

---
+++ Exercises & Assignments/ES6/ES1.1 Practice Set.js
@@ -0,0 +1,79 @@
+//Question 1
+// let age=20;
+// console.log(age);
+// age=22;
+// console.log(age);
+
+//Question 2
+// const num1=20;
+// const num2=5;
+
+// const product = num1 * num2;
+// console.log(product);
+
+// const difference = num1 - num2;
+// console.log(difference);
+
+//Question 3
+// const numberOfGates = 5;
+// numberOfGates = 6;
+// console.log(numberOfGates);
+
+//Question 4
+// const person = {
+//   name: "John",
+//   age: 20
+// }
+// console.log(person)
+// person.age= 31;
+// console.log(person);
+
+//Question 5
+// const colors ={
+//   primary: "red",
+//   secondary: "blue"
+// }
+// console.log(colors);
+// colors.tertiary="green";
+// console.log(colors);
+
+//Question 6
+// const numbers = [1, 2, 3, 4];
+// numbers[2] = 99;
+// console.log(numbers);
+
+//Question 7
+// const coordinates = {
+//   x:10,
+//   y:20
+// }
+// console.log(coordinates);
+// const newCoordinates =   {
+//   x:30,
+//   y:40
+// }
+// coordinates=newCoordinates
+
+//Question 8
+// let obj = {
+//   a:1,
+//   b:2
+// }
+// const obj2 = {
+//   a:3,
+//   b:4
+// }
+// obj = obj2
+// console.log(obj);
+// console.log(obj2)
+
+//Question 9 
+// const numericVar = 2;
+// numericVar = 3;
+
+//Question 10
+const greeting = "Hello";
+console.log(greeting)
+let name = "  Jay";
+name = "  Shawn";
+console.log(greeting + name);
