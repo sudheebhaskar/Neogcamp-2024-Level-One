35851d80cb2e9e501c1124bdc2058a5ef89177de
Sudheendra Bhaskar <sudheebhaskar@gmail.com>
2024-10-08 17:26:23 +0530

    Arrow function examples

---
+++ Exercises & Assignments/ES6/ES2.1_CW.js
@@ -0,0 +1,21 @@
+//traditional function
+function sub(a, b){
+    return a - b;
+  }
+  console.log(sub(3,8));
+  
+  
+  //addition using arrow function
+  const add = (a, b) => a + b
+  console.log(add(10, 4));
+  
+  //multiplication using arrow function
+  const product = (a, b) => a * b 
+  console.log(product(4, 2));
+  
+  //concatenation using arrow function
+  const concatenate = (str1, str2) => str1 + str2;
+  console.log(concatenate("Hello ", "World!"));
+  
+  
+  
